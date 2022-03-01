8942ce47a156f264f234dfeae38e386d56071422
kogbak <94683875+kogbak@users.noreply.github.com>
2022-03-01 15:02:23 +0100

    aucune idee

---
+++ src/components/MovieHome.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+
+    
+
+</template>
+
+<script>
+export default {
+  name: 'Liste de film',
+  props: {
+    msg: String
+  }
+}
+</script>
